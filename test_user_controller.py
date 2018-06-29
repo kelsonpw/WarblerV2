b090b26f359694bb3222c3ce03140efdbca28573
Kelson Warner <kelsonpw35@gmail.com>
2018-06-29 15:51:46 -0700

    testing for users written out

--- test_user_controller.py
+++ test_user_controller.py
@@ -4,7 +4,10 @@ import unittest
 
 
 class BaseTestCase(unittest.TestCase):
+    client = app.test_client()
+
     def setUp(self):
+        """ set up, create 2 users and commit to test db """
         app.config[
             "SQLALCHEMY_DATABASE_URI"] = 'postgres://localhost/warbler_test_db'
         app.config['TESTING'] = True
@@ -22,53 +25,77 @@ class BaseTestCase(unittest.TestCase):
         db.session.commit()
 
     def tearDown(self):
+        """ teardown after tests run """
         db.session.close()
         db.drop_all()
 
-    # def login(self, username, password):
-    #     client = app.test_client()
-    #     return client.post(
-    #         '/login',
-    #         data={
-    #             'username': 'username',
-    #             'password': 'password'
-    #         },
-    #         follow_redirects=True)
+    def login(self, username, password):
+        """ login method for users tests """
+        return self.client.post(
+            '/login',
+            data=dict(username=username, password=password),
+            follow_redirects=True)
+
+    def logout(self):
+        """ logout method for users tests """
+        return self.client.get('/logout', follow_redirects=True)
+
+    def test_login_logout(self):
+        """ testing login and logout for users """
+        result = self.login('kelson', 'password')
+        self.assertIn(b'Hello, kelson!', result.data)
+        result = self.logout()
+        self.assertIn(b'You have successfully logged out.', result.data)
 
 # Test Root
 
     def test_root(self):
-        client = app.test_client()
-        result = client.get("/")
+        """ testing root page for guests """
+        result = self.client.get("/")
+
         self.assertIn(b'<h4>New to Warbler?</h4>', result.data)
 
-#login
 
-    def test_login(self):
-        client = app.test_client()
-        result = client.post(
-            '/login',
-            data={
-                'username': 'kelson',
-                'password': 'password'
-            },
-            follow_redirects=True)
-        self.assertIn(b'Hello, kelson!', result.data)
-        result = client.get('/logout', follow_redirects=True)
-        self.assertEqual(result.status_code, 200)
-
-
-#Root when loged in
+# #Root when loged in
 
     def test_loggedin_root(self):
-        client = app.test_client()
-        client.post(
-            '/login', data={
+        """ testing root page for logged in user """
+        self.login('kelson', 'password')
+        result = self.client.get('/')
+        self.assertIn(b'<p>@kelson</p>', result.data)
+
+    def test_user_create(self):
+        """ testing signup for users """
+        result = self.client.post(
+            '/signup',
+            data={
+                'username': 'usercreate',
+                'password': 'testing',
+                'email': 'email@testuser.com'
+            },
+            follow_redirects=True)
+        self.assertIn(b'usercreate', result.data)
+
+    def test_user_update(self):
+        """ edit method for users tests """
+        self.login('kelson', 'password')
+        result = self.client.patch(
+            '/users/1',
+            data={
                 'username': 'kelson',
-                'password': 'password'
-            })
-        result = client.get('/')
-        self.assertIn(b'<p class="small">Messages</p>', result.data)
+                'password': 'password',
+                'email': 'updatedemail@gmail.com'
+            },
+            follow_redirects=True)
+        self.assertIn(b'updatedemail@gmail.com', result.data)
+
+    def test_user_destroy(self):
+        """ delete method for users tests """
+        self.login('kelson', 'password')
+        result = self.client.delete('/users/1', follow_redirects=True)
+        testuser = User.query.all()[0]
+        self.assertNotEqual(testuser.username, 'kelson')
+        self.assertNotEqual(testuser.id, '1')
 
 if __name__ == '__main__':
     unittest.main()
